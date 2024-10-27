55c07fba57f0f2690cb96f19db4d3686b879c30c
Mike DEV <vacacionesvillavicencio@gmail.com>
2024-10-27 16:31:28 -0500

    avances 27/10/2024

--- Practifcas.js
+++ Practifcas.js
@@ -1,9 +1,52 @@
-const inputUno = document.querySelector('#input-uno')
-const inputDos = document.querySelector('#input-dos')
-const btnSumar = document.querySelector('#bton-sumar')
-const resultado = document.querySelector('#resultado')
+const desktopMenu = document.querySelector('.desktop-menu')
+const UserMenu = document.querySelector('.navbar-email-address')
+const mobileMenu = document.querySelector('.mobile-menu')
+const UserMenuMobile = document.querySelector('.menu')
+const toggleThemeDesktop = document.querySelector('.toggle-theme-desktop')
+const toggleThemeMobile = document.querySelector('.toggle-theme-mobile')
+const themeAdvisor = document.querySelector('.theme-advisor')
+const themeAdvisorMobile = document.querySelector('.theme-advisor-mobile')
+const body = document.querySelector('body')
 
-btnSumar.addEventListener('click', () => {
-   resultado.innerHTML = parseInt(inputUno.value)+parseInt(inputDos.value)
+//Toggle-DesktopMenu
+UserMenu.addEventListener('click', (event)=>{
+   event.preventDefault()
+   desktopMenu.classList.toggle('inactive')
 })
 
+//toggle-MobileMenu
+UserMenuMobile.addEventListener('click', ()=>{
+   mobileMenu.classList.toggle('inactive')
+})
+
+//toggle-Theme
+const localStorageTheme = localStorage.getItem('Black-theme')
+
+toggleThemeDesktop.addEventListener('click', turntheme)
+toggleThemeMobile.addEventListener('click', turntheme)
+
+function turntheme(e){
+   e.preventDefault()
+   body.classList.toggle('blacktheme')
+
+   if(body.classList.contains('blacktheme')){
+      themeAdvisor.textContent = 'On'
+      themeAdvisorMobile.textContent = 'On'
+   } else{
+      themeAdvisor.textContent = 'Off'
+      themeAdvisorMobile.textContent = 'Off'
+   }
+   
+
+   const currentTheme = body.classList.contains('blacktheme') ? 'On' : 'Off'
+   localStorage.setItem('Black-theme', currentTheme)
+}
+
+if(localStorageTheme === 'On'){
+   body.classList.add('blacktheme')
+   themeAdvisor.textContent = 'On'
+   themeAdvisorMobile.textContent = 'On'
+}else{
+   themeAdvisor.textContent = 'Off'
+   themeAdvisorMobile.textContent = 'Off'
+}
