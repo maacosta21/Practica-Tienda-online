c5c47a4e131c042d5fa9a22f7fc6360af32671f2
Mike DEV <vacacionesvillavicencio@gmail.com>
2024-10-29 14:59:19 -0500

    Updates from 29/10/2024

--- Practifcas.js
+++ Practifcas.js
@@ -11,6 +11,7 @@ const myOrder = document.querySelector('.my-order')
 const shoppingCart = document.querySelector('.Navbar-shppingcar')
 const btoDeleteItem = document.querySelector('.delete-item')
 const shoppingcartItem = document.querySelector('.shopping-cart')
+const cardsContainer = document.querySelector('.cards-container')
 
 
 
@@ -36,6 +37,8 @@ toggleThemeMobile.addEventListener('click', turntheme)
 function turntheme(e){
    e.preventDefault()
    body.classList.toggle('blacktheme')
+   myOrder.classList.toggle('blackthemeMenus')
+   desktopMenu.classList.toggle('blackthemeMenus')
 
    if(body.classList.contains('blacktheme')){
       themeAdvisor.textContent = 'On'
@@ -52,6 +55,8 @@ function turntheme(e){
 
 if(localStorageTheme === 'On'){
    body.classList.add('blacktheme')
+   myOrder.classList.add('blackthemeMenus')
+   desktopMenu.classList.add('blackthemeMenus')
    themeAdvisor.textContent = 'On'
    themeAdvisorMobile.textContent = 'On'
 }else{
@@ -67,9 +72,79 @@ shoppingCart.addEventListener('click', (event)=>{
    mobileMenu.classList.add('inactive')
 })
 
-//shopping cart listeners
+//shoppingcar listeners
 
 myOrder.addEventListener('click', (event) => {
-   event.target.classList.contains('delete-item')
-   event.target.closest('.shopping-cart').remove()
+   if(event.target.classList.contains('delete-item')){
+      deleteItems(event, '.shopping-cart')
+   }
 })
+
+function deleteItems(event, domElement) {
+   event.target.closest(domElement).remove()
+}
+
+//Create Products DEMO(No API)
+
+productList = []
+
+productList.push(
+   {
+      name: 'cycle',
+      price: 150,
+      img: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&w=600"
+   }
+)
+
+productList.push(
+   {
+      name: 'computer',
+      price: 250,
+      img: "https://images.pexels.com/photos/1779487/pexels-photo-1779487.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+   }
+)
+
+productList.push(
+   {
+      name: 'Tv Retro',
+      price: 350,
+      img: "https://images.pexels.com/photos/6976094/pexels-photo-6976094.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+   }
+)
+
+function ProductBuilder(products){
+   products.forEach(product => {
+      const divContainer = document.createElement('div')
+      divContainer.classList.add('Product-card')
+   
+      const productImg = document.createElement('img')
+      productImg.setAttribute('src', product.img)
+   
+      const divProductInfo = document.createElement('div')
+      divProductInfo.classList.add('product-info')
+   
+      const divInfo = document.createElement('div')
+   
+      const ProductPrice = document.createElement('p')
+      ProductPrice.textContent = '$ ' +product.price
+   
+      const ProductName = document.createElement('p')
+      ProductName.textContent = product.name
+   
+      const ProductFigureContainer = document.createElement('figure')
+   
+      const imgIcon = document.createElement('img')
+      imgIcon.setAttribute('src', './icons/bt_add_to_cart.svg')
+   
+      divContainer.appendChild(productImg)
+      divProductInfo.appendChild(divInfo)
+      divInfo.appendChild(ProductPrice)
+      divInfo.appendChild(ProductName)
+      divContainer.appendChild(divProductInfo)
+      divProductInfo.appendChild(ProductFigureContainer)
+      ProductFigureContainer.appendChild(imgIcon)
+      cardsContainer.appendChild(divContainer)
+   })
+}
+
+ProductBuilder(productList)
